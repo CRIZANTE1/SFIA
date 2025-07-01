f4be5a3c334f6b157f1ced1164415f87cbd138f5
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-07-01 15:50:48 -0300

    Update 5_Mapa_do_SCI.py

--- pages/5_Mapa_do_SCI.py
+++ pages/5_Mapa_do_SCI.py
@@ -88,7 +88,7 @@ def show_map_page():
         tamanho_do_ponto = 0.3
 
         # 3. Exibe o mapa com o tamanho personalizado
-        st.map(map_data, zoom=16, size=tamanho_do_ponto, color="#0044ff")
+        st.map(map_data, zoom=16, size=tamanho_do_ponto)
 
         with st.expander("Ver detalhes dos equipamentos no mapa"):
             st.dataframe(
